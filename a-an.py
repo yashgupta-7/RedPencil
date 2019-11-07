23eb2df979c3f77253bc9d5487b9e9abb66d9080
pratyush1019 <pratyush1019@gmail.com>
2019-11-07 13:54:19 +0530

    lower case and bug fix

--- a-an.py
+++ a-an.py
@@ -7,13 +7,13 @@ from nltk.tag import pos_tag
 from nltk.corpus import wordnet as wn
 import json
 import urllib.request as urllib2
-import requests
+# import requests
 import string
 import re
 from collections import Counter
 from nltk.corpus import words
 # nltk.download('words')
-#nltk.download('cmudict')
+# nltk.download('cmudict')
 
 arpabet = nltk.corpus.cmudict.dict()
 # for word in ('barbels', 'barbeque', 'barbequed', 'barbequeing', 'barbeques'):
@@ -22,22 +22,26 @@ arpabet = nltk.corpus.cmudict.dict()
 filename=sys.argv[1]
 file=open(filename,"r")
 sentence = file.read()
+sentence=sentence.lower()
+# print(sentence)
 inpwords = word_tokenize(sentence)
 l=['A','E','I','O','U']
 pot=[]
 for i in range(0,len(inpwords)):
 	# print(i)
 	# print(inpwords[i])
-		
+	if(i==len(inpwords)-1):
+		continue
 	if (inpwords[i]=='a') :
 		# print(inpwords[i])
 		w=arpabet[inpwords[i+1]]
-		# print(w[0][0])
-		if(w[0][0]  in l):
+		# print(w)
+		# print(w[0][0][0])
+		if(w[0][0][0]  in l):
 			print("Suggestion: an "+inpwords[i+1])
 	elif(inpwords[i]=='an'):
 		w=arpabet[inpwords[i+1]]
-		if(w[0][0] not in l):
+		if(w[0][0][0] not in l):
 			print("Suggestion: a "+inpwords[i+1])
 		
 
