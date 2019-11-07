20fb2256706c64656553ccf56c99ce463e16d58e
vrindajindal2003 <vrindajindal2003@gmail.com>
2019-11-07 08:12:43 +0530

    grammar a-an added

---
+++ a-an.py
@@ -0,0 +1,61 @@
+import nltk
+import sys
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import wordnet as wn
+import json
+import urllib.request as urllib2
+import requests
+import string
+import re
+from collections import Counter
+from nltk.corpus import words
+# nltk.download('words')
+#nltk.download('cmudict')
+
+arpabet = nltk.corpus.cmudict.dict()
+# for word in ('barbels', 'barbeque', 'barbequed', 'barbequeing', 'barbeques'):
+#     print(arpabet[word])
+
+filename=sys.argv[1]
+file=open(filename,"r")
+sentence = file.read()
+inpwords = word_tokenize(sentence)
+l=['A','E','I','O','U']
+pot=[]
+for i in range(0,len(inpwords)):
+	# print(i)
+	# print(inpwords[i])
+		
+	if (inpwords[i]=='a') :
+		# print(inpwords[i])
+		w=arpabet[inpwords[i+1]]
+		# print(w[0][0])
+		if(w[0][0]  in l):
+			print("Suggestion: an "+inpwords[i+1])
+	elif(inpwords[i]=='an'):
+		w=arpabet[inpwords[i+1]]
+		if(w[0][0] not in l):
+			print("Suggestion: a "+inpwords[i+1])
+		
+
+		# pot.append(i[0:-1].lower())
+		# print(pot)
+	# 	for j in pot:
+	# 		if (j not in words) :
+	# 			suggested_words=sorted(edit1(j),key=P,reverse=True)
+	# 			print("misspelled word:",j)
+	# 			if(len(suggested_words)<=5):
+	# 				print("sugestions:",suggested_words)
+	# 			else:
+	# 				print("suggestions:",suggested_words[:5])
+
+	# 			# print("suggestions:",sorted(edit1(j),key=P,reverse=True))
+	# 	pot=[]
+	# else:
+	# 	if(i.isalpha()):
+	# 		pot.append(i.lower())
+
+
