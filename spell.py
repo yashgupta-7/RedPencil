9d288e2cba3a851455f701bf0f05e6354ee5ccca
pratyush1019 <pratyush1019@gmail.com>
2019-11-25 12:36:51 +0530

    spell check contextual

--- spell.py
+++ spell.py
@@ -2,6 +2,11 @@ import nltk
 import sys
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
+from nltk.corpus import wordnet as wn
+import json
+import urllib.request as urllib2
+import requests
+import string
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import wordnet as wn
@@ -12,14 +17,18 @@ import string
 import re
 from collections import Counter
 from nltk.corpus import words
+import string
+
+punct = string.punctuation
 # nltk.download('words')
-filename=sys.argv[1]
+# filename=sys.argv[1]
 def conv(text): return re.findall(r'\w+', text.lower())
 
 WORDS = Counter(conv(open('big.txt').read()))
-
+file=open("google-10000-english.txt","r")
+words=file.read().split()
 # uniq_words = set(open('dictionary.txt').read().split())
-words=[i.lower() for i in words.words()]
+# words=[i.lower() for i in words.words()]
 # words=[i.lower() for i in uniq_words]
 # print(words)
 def P(word, N=sum(WORDS.values())): 
@@ -51,9 +60,7 @@ def edit2(word):
 	return set([j for i in edit1(word) for j in edit1(i) ])
 
 # print(filename)
-file=open(filename,"r")
-sentence = file.read()
-inpwords = pos_tag(word_tokenize(sentence))
+# inpwords = pos_tag(word_tokenize(sentence))
 # print(inpwords)
 
 # for i in range(len(inpwords)):
@@ -65,26 +72,131 @@ inpwords = pos_tag(word_tokenize(sentence))
 
 # 	(w,t) = inpwords[i]
 # 	print(w,t)
+def getSynonyms(wordBefore, data, wordAfter):
+	res=[]
+	for i in data:
+		wd = i
+		if (True):
+			trigram = []
+			if (wordBefore!="" and (wordBefore not in punct)):
+				trigram.append(wordBefore)# trigram = [trigram wordBefore]
+			trigram.append(wd)# trigram = [trigram wd]
+			if (wordAfter!="" and (wordAfter not in punct)):
+				trigram.append(wordAfter)# trigram = [trigram wordAfter]
+			f = phraseFreqFinder("%20".join(trigram))
+			if (f>1000):
+				res.append((f,wd))
+	return res
+
+def getSynonymsFirstWord(data, wordAfter, wordAfterAfter):
+	res = []
+	for i in data:
+		wd = i
+		if (True):
+			trigram = []
+			trigram.append(wd)
+			if (wordAfter!="" and (wordAfter not in punct)):
+				trigram.append(wordAfter)# trigram = [trigram wordBefore]
+			if (wordAfterAfter!="" and (wordAfterAfter not in punct)):
+				trigram.append(wordAfterAfter)# trigram = [trigram wordAfter]
+			f = phraseFreqFinder("%20".join(trigram))
+			if (f>1000):
+				res.append((f,wd))
+	return res
+
+
+def getSynonymsEndWord(wordBeforeBefore, wordBefore, data):
+	res = []
+	for i in data:
+		wd = i['word']
+		if (True):
+			trigram = []
+			if (wordBeforeBefore!="" and (wordBeforeBefore not in punct)):
+				trigram.append(wordBeforeBefore)# trigram = [trigram wordAfter]
+			if (wordBefore!="" and (wordBefore not in punct)):
+				trigram.append(wordBefore)# trigram = [trigram wordBefore]
+			trigram.append(wd)
+			f = phraseFreqFinder("%20".join(trigram))
+			if (f>1000):
+				res.append((f,wd))
+	return res
+
+def phraseFreqFinder(phrase):
+	api_url = 'https://api.phrasefinder.io/search?corpus=eng-us&topk=1&format=tsv&query=' + phrase
+	response = requests.get(api_url)
+	data = response.text
+	try:
+		x = data.split('\t')[1]
+		return int(x)
+	except:
+		return 0
 
 pot=[]
-for i,j in inpwords:
+# file=open(filename,"r")
+# sentence = file.read()
+inpwords = pos_tag(word_tokenize(input()))
+ln=len(inpwords)
+for i in range(ln):
 	# print(i)
-	if (i=='.' or i=='?' or i == '!') :
-		# pot.append(i[0:-1].lower())
-		# print(pot)
-		for j in pot:
-			if (j not in words) :
-				suggested_words=sorted(edit1(j),key=P,reverse=True)
-				print("misspelled word:",j)
-				if(len(suggested_words)<=5):
-					print("sugestions:",suggested_words)
-				else:
-					print("suggestions:",suggested_words[:5])
+	(w,t)=inpwords[i]
+	j=w
+	if (w.isalpha() and w not in words) :
+		suggested_words=sorted(edit1(j),key=P,reverse=True)
+		# print(suggested_words)
+		print("misspelled word:",j)
+		if(len(suggested_words)<=5):
+			print("sugestions:",suggested_words)
+		else:
+			suggested_words=suggested_words[:5]
+		if (i==0):
+			if (ln>=3):
+				wa = inpwords[i+1][0]
+				waa = inpwords[i+2][0]
+			elif (ln==2):
+				wa = inpwords[i+1][0]
+				waa = ""
+			else:
+				wa = ""
+				waa = ""
+			if (True):
+				syns = getSynonymsFirstWord(suggested_words,wa,waa)
+				syns.sort(reverse=True)
+				# print(syns)
+				if (len(syns)!=0):
+					print(w,[ x[1] for x in syns ])
+			continue
+
+		elif (i==len(inpwords)-1):
+			if (ln>=3):
+				wb = inpwords[i-1][0]
+				wbb = inpwords[i-2][0]
+			elif (ln==2):
+				wb = inpwords[i-1][0]
+				wbb = ""
+			else:
+				wb = ""
+				wbb = ""
+			if (True):
+				syns = getSynonymsEndWord(wbb,wb,suggested_words)
+				syns.sort(reverse=True)
+				# print(syns)
+				if (len(syns)!=0):
+					print(w,[ x[1] for x in syns ])
+			continue
+
+		(wb,tb) = inpwords[i-1]
+		(wa,ta) = inpwords[i+1]
+		if (True):
+			syns = getSynonyms(wb,suggested_words,wa)
+			syns.sort(reverse=True)
+			# print(syns)
+			if (len(syns)!=0):
+				print(w,[ x[1] for x in syns ])	
+
+		# print("suggestions:",suggested_words)
 
 				# print("suggestions:",sorted(edit1(j),key=P,reverse=True))
-		pot=[]
-	else:
-		if(i.isalpha()):
-			pot.append(i.lower())
+		# pot=[]
+
 
 
